f830cc51246d6fd7b3b85c8e1fbe753ee6f618f6
Polatcan Gürbüz <115667909+polatcangurbuz@users.noreply.github.com>
2022-10-13 14:32:36 +0300

    Update dıkdortgencevre.java

--- dıkdortgencevre.java
+++ dıkdortgencevre.java
@@ -1,7 +1,7 @@
 package com.aaa.dikdortgencevre;
 
 public class dıkdortgencevre {
-    public void cevre(double sayi1,double sayi2){
+    public void cevre(double sayi1,double sayi2){//void'den dolayıgeri değer döndürmeyen metod
         double cev=2*sayi1+2*sayi2;
         System.out.println("Dikdörtgenin çevre uzunluğu : "+cev);
     }
