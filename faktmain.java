6f3600398da1e378c54a9bbf235429a442337f40
Polatcan Gürbüz <115667909+polatcangurbuz@users.noreply.github.com>
2022-10-13 15:16:44 +0300

    Update faktmain.java

--- faktmain.java
+++ faktmain.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class faktmain {
     public static void main(String args[]){
-        Scanner tarayici=new Scanner(System.in);
+        Scanner tarayici=new Scanner(System.in);//kullanıcıdan veri girişi için
         System.out.println("Faktöriyelini hesaplamak istediğiniz sayiyi giriniz : ");
         int sayi= tarayici.nextInt();
-        fakt f1=new fakt();
+        fakt f1=new fakt();//diğer sınıftaki verileri alabilmek için
         int faktoriyel=f1.faktor(sayi);
     }
 }
