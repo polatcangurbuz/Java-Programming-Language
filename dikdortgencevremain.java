422290de6ada74fc234aebe0ff47337f4b450c96
Polatcan Gürbüz <115667909+polatcangurbuz@users.noreply.github.com>
2022-10-13 14:31:33 +0300

    Update dikdortgencevremain.java

--- dikdortgencevremain.java
+++ dikdortgencevremain.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class dikdortgencevremain {
     public static void main(String args[]){
-    Scanner tarayici = new Scanner(System.in);
+    Scanner tarayici = new Scanner(System.in);//kullanıcıdan veri alma
         System.out.println("Dikdörtgenin kısa kenar uzunluğu : ");
         int sayi1= tarayici.nextInt();
         System.out.println("Dikdörtgenin uzun kenar uzunluğu : ");
         int sayi2= tarayici.nextInt();
-        dıkdortgencevre d=new dıkdortgencevre();
+        dıkdortgencevre d=new dıkdortgencevre();//nesne yönelimli programada farklı sınıftan veri alma
         d.cevre(sayi1,sayi2);
     }
 }
